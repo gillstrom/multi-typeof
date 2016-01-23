4aada3ee84cb0156e309b0f265b945b00af72aab
Andreas Gillström <andreasgillstrom@gmail.com>
2016-01-23 10:48:16 +0100

    Use `AVA` in favour of `mocha`

--- test.js
+++ test.js
@@ -1,17 +1,16 @@
-'use strict';
-var assert = require('assert');
-var multiTypeof = require('./');
+import test from 'ava';
+import m from './';
 
-it('Should return true if the input is one of the types', function () {
-	assert.strictEqual(multiTypeof('hello', ['string', 'array']), true);
-	assert.strictEqual(multiTypeof(['hi', 123], ['string', 'array']), true);
-	assert.strictEqual(multiTypeof(true, ['string', 'array', 'boolean']), true);
-	assert.strictEqual(multiTypeof({foo: 'bar'}, 'object'), true);
-	assert.strictEqual(!multiTypeof(123, ['string', 'array']), true);
+test('Should return true if the input is one of the types', t => {
+	t.true(m('hello', ['string', 'array']));
+	t.true(m(['hi', 123], ['string', 'array']));
+	t.true(m(true, ['string', 'array', 'boolean']));
+	t.true(m({foo: 'bar'}, 'object'));
+	t.true(!m(123, ['string', 'array']));
 });
 
-it('Should return false if the input is not one of the types', function () {
-	assert.strictEqual(multiTypeof({foo: 'bar'}, ['string', 'array']), false);
-	assert.strictEqual(multiTypeof(true, ['string', 'object']), false);
-	assert.strictEqual(multiTypeof(['foo'], ['object']), false);
+test('Should return false if the input is not one of the types', t => {
+	t.false(m({foo: 'bar'}, ['string', 'array']));
+	t.false(m(true, ['string', 'object']));
+	t.false(m(['foo'], ['object']));
 });
